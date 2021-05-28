e577ba841f8a9ab7a836b7810a248742a175d27f
igece <igcelis@gmail.com>
2021-05-28 23:55:10 +0200

    Changed parameters denomination

--- src/ObjDiff/Extensions.cs
+++ src/ObjDiff/Extensions.cs
@@ -5,15 +5,15 @@ namespace ObjDiff
 {
   public static class Extensions
   {
-    public static IEnumerable<Difference> Diff<T>(this T obj, T other, CompareOptions compareOptions = null) where T : class
+    public static IEnumerable<Difference> Diff<T>(this T left, T right, CompareOptions compareOptions = null) where T : class
     {
-      return ObjDiff.Diff(obj, other, compareOptions);
+      return ObjDiff.Diff(left, right, compareOptions);
     }
 
 
-    public static void Patch<T>(this T obj, IEnumerable<Difference> differences) where T : class
+    public static void Patch<T>(this T target, IEnumerable<Difference> differences) where T : class
     {
-      ObjDiff.Patch(obj, differences);
+      ObjDiff.Patch(target, differences);
     }
   }
 }
